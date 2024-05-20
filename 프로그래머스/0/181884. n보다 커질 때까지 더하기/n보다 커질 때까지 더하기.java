21544f2bf320eaf94eca4cb696429268ddfd065d
cyseok <129164621+cyseok@users.noreply.github.com>
2024-05-21 03:56:29 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -1,13 +1,25 @@
 class Solution {
     public int solution(int[] numbers, int n) {
+         
         int answer = 0;
+        // 향상된 for문
+        // 장점
+        // 배열 인덱스 문제 해결 (ArrayIndexOutOfBoundsException 예외를 피할 수 있다.)
         
-        for(int i=0; i<numbers.length; i++) {
-            answer += numbers[i];
+        // 단점
+        // 배열이나 ArrayList 값을 사용할 순 있지만 절대 수정할 수는 없다.
+        
+        // 사용
+        // 1. ArrayList 원소 출력
+        // 2. 배열 원소 출력
+
+        for(int num: numbers) {
+            answer += num;
+
+            if(answer > n) 
+                break;
             
-            if(answer > n) break;
         }
-        
         return answer;
     }
 }
