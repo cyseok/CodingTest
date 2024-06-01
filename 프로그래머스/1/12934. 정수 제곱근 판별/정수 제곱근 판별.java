06529cd66c78cea9bacdb821b8bb17cdf0af21f4
cyseok <129164621+cyseok@users.noreply.github.com>
2024-06-01 23:06:05 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,8 +1,8 @@
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        
-        for(long i=1; i<=n; i++) {
+        /*
+        for(long i=1; i*i<=n; i++) {
             if(i*i == n) {
                 answer = (i+1)*(i+1);
                 break;
@@ -10,7 +10,14 @@ class Solution {
                 answer = -1;
             }
         }
+        */
         
-        return answer;
+        // Math 클래스의 sqrt, pow 메소드 사용
+        long sqrt = (long) Math.sqrt(n);
+        if(n == Math.pow(sqrt,2)) {
+            return (long)Math.pow(sqrt+1, 2);
+        } else {
+        	return -1;
+        }
     }
 }
