2704ba94f79ad8e834a126c47ad9a5a771497dcf
cyseok <129164621+cyseok@users.noreply.github.com>
2024-06-13 01:32:20 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.12 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,14 +1,15 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
+        int a = s.length();
         //System.out.println(s.length());
         
         // 길이가 짝수, 홀수일 때
         if(s.length() % 2 == 0) {
-            answer = s.substring((s.length()/2)-1, (s.length()/2)+1);
+            answer = s.substring((a/2)-1, (a/2)+1);
             System.out.println(answer);
         } else {
-            answer = s.substring((s.length()-1)/2, (s.length()+1)/2);
+            answer = s.substring((a-1)/2, (a+1)/2);
             System.out.println(answer);
         }
         return answer;
