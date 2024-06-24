033abfdd90a0dd78c4670ae0f309d3414b855cd9
cyseok <129164621+cyseok@users.noreply.github.com>
2024-06-25 04:28:37 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 78.00 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        // Stack<Integer> stack = new Stack<>();
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for(int i : arr) {
+            // 향상된 for문에서 선언된 변수는 배열을 하나씩(모두) 반환한다!
+            
+            // peekLast() : // 데크의 마지막 요소를 반환 -> 덱이 비어있는 경우 예외를 발생시키지 않고 null을 반환
+            if(deque.peekLast() == null || deque.peekLast() != i) {
+                 // addLast() : // 데크 맨 끝에 요소를 추가
+                 deque.addLast(i);
+            } 
+        }
+        
+        int[] answer = new int[deque.size()];
+        
+        for (int i = 0; i < answer.length; i++) {
+            // pollFirst() : // 데크 첫번째 요소를 반환하고 제거
+            answer[i] = deque.pollFirst();
+        }
+
+        return answer;
+    }
+}
