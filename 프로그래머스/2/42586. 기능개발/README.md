ccc173c06764f602fb229a09b6b549aa38409503
cyseok <129164621+cyseok@users.noreply.github.com>
2024-06-26 22:08:54 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 73.3 MB, 시간: 0.24 ms
+메모리: 77.6 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 26일 07:40:59
+2024년 06월 26일 22:08:51
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,26 +2,29 @@ import java.util.*;
 
 class Solution {
     public List<Integer> solution(int[] progresses, int[] speeds) {
-        
-        
+    
         ArrayList<Integer> answer = new ArrayList<Integer>();
         Deque<Integer> deque = new ArrayDeque<>();
+    
         int cnt = 0;
         int speedsIndex = 0;
         int dequeSize = 0;
-        
-       
+    
         for(int i=0; i<progresses.length; i++) {
-            deque.addLast(progresses[i]);
+                // addLast() : 값을 데크의 마지막에 추가
+                deque.addLast(progresses[i]);
         }
-      
-        dequeSize = deque.size();
         
+        // 이후에 데크의 값을 빼주기 때문에 변동이 생겨, 최초 데크의 길이를 선언했다.
+        dequeSize = deque.size();
+    
         while(!deque.isEmpty()) {
-            
+    
+            // peekFirst() : 데크의 첫번째 요소 반환
             while(deque.peekFirst() < 100) {
                 for(int i=speedsIndex; i<dequeSize; i++) {
-                    
+                    // 첫번째 요소를 스피드랑 더해서 마지막 요소에 저장
+                    // 100 미만이라면 speeds를 더해주고 그 외는 요소값을 그대로
                     if(deque.peekFirst() < 100) {
                         deque.addLast(deque.pollFirst() + speeds[i]);
                         
@@ -30,23 +33,24 @@ class Solution {
                     }
                 }
             }
-            
+        
+            // 첫번째 요소가 100이상이라면 첫번째 요소를 제거하고 배포 횟수(cnt)를 1 증가한다.
             while(deque.peekFirst() >= 100) {
                 deque.pollFirst();
-                cnt++;
+                cnt++;   
+                // 데크를 뺀 만큼 speedsIndex도 추가해줘 배열의 index 순서도 맞춰준다.
                 speedsIndex++;
-                
-                
+                // 마지막 요소를 제거했다면 break; 
                 if(deque.peekFirst() == null) break;
                 
             }
-            System.out.println(speedsIndex);
+            // 배포 횟수를 List에 추가하고
             answer.add(cnt);
+            // 다시 0으로 초기화
             cnt = 0;
-            
+            // 데크가 비면 break; 
             if(deque.isEmpty()) break;
-            
-        }
+        }  
         
         return answer;
     }
