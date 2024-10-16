3fe3486e4822ab84d5a2787f9cc643f2d69eadd7
cyseok <129164621+cyseok@users.noreply.github.com>
2024-10-16 20:56:05 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int n) {
+        /*
         int answer = 0;
         
         answer = (n / 7);
@@ -7,6 +8,9 @@ class Solution {
         if(n%7 > 0) {
             answer += 1;
         }
+        */
+        // 삼항연산자 사용
+        int answer = (n%7 == 0) ? n/7 : (n/7) + 1;
         
         return answer;
     }
